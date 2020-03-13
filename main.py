24691534436e934f855cd9fbdf2b5edb5ae6c118
jlove29 <juliette29love@gmail.com>
2020-03-13 09:50:23 -0700

    final system

--- main.py
+++ main.py
@@ -54,7 +54,9 @@ def perform(f, init):
 
     # 1. Vanilla Gradient Descent
     alg_GD = GD(f)
-    min_GD, t_GD = alg_GD.perform(init, a=0.015)
+    min_GD, t_GD = alg_GD.perform(init, a=0.01)
+    if t_GD == 0:
+        t_GD = 10000
     #write_matrix(min_GD, 'GD')
     min_GD = min_GD.tolist()
     min_GD = {'x': min_GD[0], 'y': min_GD[1], 't': t_GD}
@@ -62,30 +64,40 @@ def perform(f, init):
     # 2. Gradient Descent with Momentum
     alg_GDm = GD_m(f)
     min_GDm, t_GDm, mu_GDm = alg_GDm.perform(init, validation=False, a=0.01)
+    if t_GDm == 0:
+        t_GDm = 10000
     #write_matrix(min_GDm, 'GDm')
     min_GDm = min_GDm.tolist()
     min_GDm = {'x': min_GDm[0], 'y': min_GDm[1], 't': t_GDm}
 
     # 3. AdaGrad
     alg_AdaGrad = AdaGrad(f)
-    min_AdaGrad, t_AdaGrad = alg_AdaGrad.perform(init, a=0.3)
+    min_AdaGrad, t_AdaGrad = alg_AdaGrad.perform(init, a=0.1)
+    if t_AdaGrad == 0:
+        t_AdaGrad = 10000
     #write_matrix(min_AdaGrad, 'AdaGrad')
     min_AdaGrad = min_AdaGrad.tolist()
     min_AdaGrad = {'x': min_AdaGrad[0], 'y': min_AdaGrad[1], 't': t_AdaGrad}
 
     # 4. RMSProp
     alg_RMSProp = RMSProp(f)
-    min_RMSProp, t_RMSProp, param = alg_RMSProp.perform(init, validation=False, a=0.008)
+    min_RMSProp, t_RMSProp, param = alg_RMSProp.perform(init, validation=False, a=0.01)
+    if t_RMSProp == 0:
+        t_RMSProp = 10000
     #write_matrix(min_RMSProp, 'RMSProp')
     min_RMSProp = min_RMSProp.tolist()
     min_RMSProp = {'x': min_RMSProp[0], 'y': min_RMSProp[1], 't': t_RMSProp}
 
     # 5. Adam
     alg_Adam = Adam(f)
-    min_Adam, t_Adam, a, b = alg_Adam.perform(init, validation=False, a=0.1)
+    min_Adam, t_Adam, a, b = alg_Adam.perform(init, validation=False, a=0.05)
+    if t_Adam == 0:
+        t_Adam = 10000
     #write_matrix(min_Adam, 'Adam')
     min_Adam = min_Adam.tolist()
     min_Adam = {'x': min_Adam[0], 'y': min_Adam[1], 't': t_Adam}
+
+    print(t_GD, t_GDm, t_AdaGrad, t_RMSProp, t_Adam)
 
     return {
             'GD': {'steps': min_GD, 'time': t_GD},
